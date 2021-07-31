b9aba2113f5a2f1afc781da67620104d8dcf4e9a
Cameron Houck <cameronhouck1@gmail.com>
2021-07-31 17:01:38 -0400

    nesting home component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import "./App.css";
 import Navbar from "./Navbar";
+import Home from "./Home";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>Welcome to the blog!</h1>
+        <Home />
       </div>
     </div>
   );
